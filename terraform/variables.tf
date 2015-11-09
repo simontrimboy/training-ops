89ea6bd22c4bc78fc58837c16c712daddb7d196e
Jay Wallace <jaycwallace@gmail.com>
2015-11-09 13:10:10 -0800

    initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "name" { default = "training" }
+variable "key_name" { default = "training-key" }
+variable "vpc_cidr" { default = "10.139.0.0/16" }
+variable "subnet_cidr" { default = "10.139.101.0/24" }
+variable "access_key" {}
+variable "secret_key" {}
